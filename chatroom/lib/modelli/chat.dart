4dbca267efb05e516a6979c7c935344760cadb14
Victor Mereuta <victor.mereuta2@gmail.com>
2025-12-15 09:19:29 +0100

    pulizia repository

--- chatroom/lib/modelli/chat.dart
+++
@@ -1,7 +0,0 @@
-class Chat {
-  final String id;
-  final String name;
-  final String avatar;
-
-  Chat(this.id, this.name, this.avatar);
-}
--- chatroom/lib/modelli/message.dart
+++
@@ -1,7 +0,0 @@
-class Message {
-  final String text;
-  final bool isMe;
-  final DateTime timestamp;
-
-  Message(this.text, this.isMe, this.timestamp);
-}
